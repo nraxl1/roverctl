063721ab4a09979e8710e5b9c63598c88b3c4a01
nraxl1 <209858497+nraxl1@users.noreply.github.com>
2025-04-30 23:06:00 +0200

    decided to keep minimal, will test and document later

--- mutex_queue.h
+++ mutex_queue.h
@@ -2,6 +2,16 @@
 #define MUTEX_QUEUE_H
 
 #include <pthread.h>
-#include "queues.h"
+#include "queue.h"
+
+typedef struct Mutex {
+    pthread_mutex_t lock;
+    queue_t* shared_queue;
+} mutex_queue_t;
+
+void initialize_mutex_queue(mutex_queue_t* mq) {
+        pthread_mutex_init(&mq->lock, NULL);
+        initialize_queue(mq->shared_queue);
+}
 
 #endif
