b055ad3101835995f925f9387a06934983cbe792
nraxl1 <209858497+nraxl1@users.noreply.github.com>
2025-05-19 18:09:05 +0200

    Update control.c
    added yields

--- control.c
+++ control.c
@@ -1,3 +1,4 @@
+#include <pthread>
 #include "mutex_queue.h"
 
 #define TRAVERSE 0
@@ -10,11 +11,14 @@ int control(mutex_queue_t *outgoing, mutex_queue_t *incoming, pthread_mutex_lock
   int location_y = 0;
   int angle_degrees = 0; // angle from positive y-axis (towards cartesian quadrant 1)
   while (1){
+    // underlying while loops are not to exceed 100ms (standard quantum) of runtime before yielding
+    // otherwise the thread might get interrupted while accessing FPGA memory mappings, which should not lead to undefined behavior but is not advisable either.
     while (opmode == TRAVERSE){
       // decide where to go 
       // step towards going there
       // check for objects
       // change to IDENTIFY operation mode if needed
+      pthread_yield();
     }
     while (opmode == IDENTIFTY){
       // approach while checking ground
@@ -22,6 +26,7 @@ int control(mutex_queue_t *outgoing, mutex_queue_t *incoming, pthread_mutex_lock
       // when approached, check sensor data against thresholds to identify it
       // use circular motions to ID cliff/border and different movement flowchart to id small rock/large rock
       // change to TRAVERSE operation mode when object identified
+      pthread_yield();
     }
     return 0;
 }
