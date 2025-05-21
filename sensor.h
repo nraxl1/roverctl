590c51535e3992b3a952c2408db920ddd4c7e231
nraxl1 <209858497+nraxl1@users.noreply.github.com>
2025-05-21 15:05:46 +0200

    Create sensor.h

---
+++ sensor.h
@@ -0,0 +1,23 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <pthread.h>
+
+typedef struct RGB {
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+} rgb_t;
+
+typedef struct SensorFrame {
+  float distance1;
+  float distance2;
+  float distance3;
+  rgb_t color1;
+  rgb_t color2;
+  uint8_t confidence; /**< @brief increased every time the sensor thread finds the frame unflushed and averages it with another frame. set to 0 on flush. */
+} sensor_frame_t;
+
+int handle_sensors(sensor_frame_t *sensor_frame, pthread_mutex_t *sensor_mutex);
+
+#endif /* SENSOR_H */
